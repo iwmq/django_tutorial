658c938f296f9d42c3751cfff62f552507239e2a
iwmq <19398624+iwmq@users.noreply.github.com>
2020-07-21 21:08:02 +0800

    Use Vue3 RC2

--- mysite/settings.py
+++ mysite/settings.py
@@ -60,7 +60,10 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'vue3_app/dist'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,10 +136,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/_assets/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "vue3_app/dist/_assets"),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    # path('', include('polls.urls')),
+    path('', TemplateView.as_view(template_name="index.html")),
     path('polls/', include('polls.urls')),
     path('polls2/', include('polls2.urls')),
     path('admin/', admin.site.urls),
